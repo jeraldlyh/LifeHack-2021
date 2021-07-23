06dc99ad8988eeae7343d8e2986b24bd9a870e4c
oversparkling <56868622+oversparkling@users.noreply.github.com>
2021-07-24 00:51:05 +0800

    Forums page

--- screens/forums/ForumStack.js
+++ screens/forums/ForumStack.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import ForumHome from "./ForumHome";
+import ForumPage from "./ForumPage";
 
 const Forum = createStackNavigator();
 
@@ -8,7 +9,7 @@ const ForumStack = () => {
     return (
         <Forum.Navigator screenOptions={{ headerShown: false }}>
             <Forum.Screen name="ForumHome" component={ForumHome} />
-            
+            <Forum.Screen name="ForumPage" component={ForumPage} />
         </Forum.Navigator>
     )
 }
