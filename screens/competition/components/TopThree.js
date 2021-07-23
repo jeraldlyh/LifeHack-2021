407cf33195f6f566c7335a88d3d68ae1aea2c6c5
Jerald <jeraldssw@gmail.com>
2021-07-24 06:27:07 +0800

    WIP: Link of db to leaderboards

--- screens/competition/components/TopThree.js
+++ screens/competition/components/TopThree.js
@@ -3,40 +3,43 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import tailwind from "tailwind-rn";
 
 function TopThree(props) {
-    <View style={tailwind("flex flex-row justify-around mt-5")}>
-        <View style={"flex flex-col items-center justify-center"}>
-            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 16, color: "#2B2B2B" }}>2</Text>
-            <Image source={require("../../../assets/icons/down.png")} style={styles.icon} />
-            <Image source={{ uri: props.one.avatar }} style={styles.profile2} />
-            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 15, marginTop: 5 }}>{props.one.name}</Text>
-            <View style={tailwind("flex flex-row items-center justify-center")}>
-                <Image source={require("../../../assets/icons/coin.png")} style={styles.coin} />
-                <Text style={{ fontFamily: 'Poppins-Normal', fontSize: 16, color: "#2B2B2B", margin: 3 }}>{props.one.currency}</Text>
-            </View>
-        </View>
+    const { data } = props
 
-        <View style={"flex flex-col items-center justify-center"}>
-            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 16, color: "#2B2B2B" }}>2</Text>
-            <Image source={require("../../../assets/icons/down.png")} style={styles.icon} />
-            <Image source={{ uri: props.one.avatar }} style={styles.profile1} />
-            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 15, marginTop: 5 }}>{props.one.name}</Text>
-            <View style={tailwind("flex flex-row items-center justify-center")}>
-                <Image source={require("../../../assets/icons/coin.png")} style={styles.coin} />
-                <Text style={{ fontFamily: 'Poppins-Normal', fontSize: 16, color: "#2B2B2B", margin: 3 }}>{props.one.currency}</Text>
+    return (
+        <View style={tailwind("flex flex-row justify-around mt-5")}>
+            <View style={tailwind("flex flex-col items-center justify-center")}>
+                <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 16, color: "#2B2B2B" }}>2</Text>
+                <Image source={require("../../../assets/icons/down.png")} style={styles.icon} />
+                <Image source={{ uri: data[1].avatar }} style={styles.profile2} />
+                <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 15, marginTop: 5 }}>{data[1].name}</Text>
+                <View style={tailwind("flex flex-row")}>
+                    <Image source={require("../../../assets/icons/coin.png")} style={styles.coin} />
+                    <Text style={{ fontFamily: 'Poppins-Normal', fontSize: 16, color: "#2B2B2B", margin: 3 }}>{data[1].currency}</Text>
+                </View>
             </View>
-        </View>
 
-        <View style={"flex flex-col items-center justify-center"}>
-            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 16, color: "#2B2B2B" }}>2</Text>
-            <Image source={require("../../../assets/icons/down.png")} style={styles.icon} />
-            <Image source={{ uri: props.one.avatar }} style={styles.profile3} />
-            <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 15, marginTop: 5 }}>{props.one.name}</Text>
-            <View style={tailwind("flex flex-row items-center justify-center")}>
-                <Image source={require("../../../assets/icons/coin.png")} style={styles.coin} />
-                <Text style={{ fontFamily: 'Poppins-Normal', fontSize: 16, color: "#2B2B2B", margin: 3 }}>{props.one.currency}</Text>
+            <View style={tailwind("flex flex-col items-center justify-center")}>
+                <Image source={require("../../../assets/icons/crown.png")} style={styles.icon} />
+                <Image source={{ uri: data[0].avatar }} style={styles.profile1} />
+                <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 15, marginTop: 5 }}>{data[0].name}</Text>
+                <View style={tailwind("flex flex-row items-center justify-center")}>
+                    <Image source={require("../../../assets/icons/coin.png")} style={styles.coin} />
+                    <Text style={{ fontFamily: 'Poppins-Normal', fontSize: 16, color: "#2B2B2B", margin: 3 }}>{data[0].currency}</Text>
+                </View>
+            </View>
+
+            <View style={tailwind("flex flex-col items-center justify-center")}>
+                <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 16, color: "#2B2B2B" }}>3</Text>
+                <Image source={require("../../../assets/icons/up.png")} style={styles.icon} />
+                <Image source={{ uri: data[2].avatar }} style={styles.profile3} />
+                <Text style={{ fontFamily: 'Poppins-SemiBold', fontSize: 15, marginTop: 5 }}>{data[2].name}</Text>
+                <View style={tailwind("flex flex-row items-center justify-center")}>
+                    <Image source={require("../../../assets/icons/coin.png")} style={styles.coin} />
+                    <Text style={{ fontFamily: 'Poppins-Normal', fontSize: 16, color: "#2B2B2B", margin: 3 }}>{data[2].currency}</Text>
+                </View>
             </View>
         </View>
-    </View>
+    )
 };
 
 const styles = StyleSheet.create({
